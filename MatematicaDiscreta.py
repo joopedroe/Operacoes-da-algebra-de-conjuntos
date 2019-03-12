ba755a971e7b35e431f5910db743c0cc8e3cdbdb
Erick Willames <erickrodriguesmusic@hotmail.com>
2019-03-12 00:02:07 -0300

    inicial

---
+++ MatematicaDiscreta.py
@@ -0,0 +1,88 @@
+def tratarExpressao(expre):
+    expressao=expre.strip('{}')
+    return expressao.split(',')
+
+def RetratarExpre(lista):
+    expre='{'
+    for i,valor in enumerate(lista):
+        if i < len(lista)-1:
+            expre+=valor+','
+        else:
+            expre+=valor+'}'
+    return expre
+
+def Uniao(valor1,valor2):
+    for i in valor2:
+        if i not in valor1:
+            valor1.append(i)
+    return valor1
+
+def Interseccao(valor1,valor2):
+    interseccao=[]
+    for i in valor2:
+        if i in valor1:
+            interseccao.append(i)
+    return interseccao
+
+def Diferenca(valor1,valor2):
+    dife=[]
+    for i in valor2:
+        if i not in valor1:
+            dife.append(i)
+    for i in valor1:
+        if i not in valor2:
+            dife.append(i)
+    return dife
+
+def Complemento(valor1,valor2):
+    compe=[]
+    for i in valor1:
+        if i not in valor2:
+            compe.append(i)
+    return compe
+
+'''def ConjuntosDasPartes(valor1):
+    conju=['{}']
+    while cont != len(valor1):
+        valor='{'+valor1[0:i]+'}'
+        conju.append(valor)'''
+        
+def ProdutoCartisiano(valor1,valor2):
+    pro=[]
+    for i in valor1:
+        for c in valor2:
+            pr='('+i+','+c+')'
+            pro.append(pr)
+    return pro
+
+def UniaoDisjunta(valor1,valor2):
+    uniao=[]
+    for i in valor1:
+        va=i+'A'
+        uniao.append(va)
+    for i in valor2:
+        va=i+'B'
+        uniao.append(va)
+    return uniao
+
+print('Conjunto A={1,2,3,4,5,10}')
+print('Conjunto B={5,6,3,4,7,9}')
+expre1='{1,2,3,4,5,10}'
+expre2='{5,6,3,4,7,9}'
+uniao=Uniao(tratarExpressao(expre1),tratarExpressao(expre2))
+print('União: ',RetratarExpre(uniao))
+
+inter=Interseccao(tratarExpressao(expre1),tratarExpressao(expre2))
+print('Intersecção: ',RetratarExpre(inter))
+
+dife=Diferenca(tratarExpressao(expre1),tratarExpressao(expre2))
+print('Diferença :',RetratarExpre(dife))
+
+comple=Complemento(tratarExpressao(expre1),tratarExpressao(expre2))
+print('Complemento : ~B =',RetratarExpre(comple))
+
+pro=ProdutoCartisiano(tratarExpressao(expre1),tratarExpressao(expre2))
+print('Produto Cartesiano: ',RetratarExpre(pro))
+
+uniao=UniaoDisjunta(tratarExpressao(expre1),tratarExpressao(expre2))
+print('União Disjunta: A+B=',RetratarExpre(uniao))
